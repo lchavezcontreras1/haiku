5b1dc58daad1bea10bf949e1566d2309fc71c641
Lesli Chavez Contreras <lchavezcontreras1@my.wctc.edu>
2021-05-13 23:18:20 -0500

    I finished it, AT LAST! There was probably a simpler way...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Global
-//the index of the outer array indicates the number of syllable sin the word in the inner array
-//example: Les-li Let-ter stored in
+//the index of the outer array indicates the number of syllable in the word in the inner array
+//example: Les-li Let-ter stored in index 1 syllable 2
 let wordList = [
     /*0 1*/["in", "at", "the", "a"],
     /*1 2*/["over", "special", "perfect"],
@@ -10,8 +10,10 @@ let wordList = [
     /*5 6*/["responsibility", "electrifying", "identification"],
     /*6 7*/["industrialization", "artificiality", "indestructibility"],
 ]
+
+//watch out for syllable array
 let syllableArray = [];
-let row = "";
+let row = [];
 
 // document.ready
 $(document).ready(function () {
@@ -25,8 +27,11 @@ function addWord(e) {
     e.preventDefault();
     let word = $("input#word").val();
 //count the number of dashes and determine how many syllables
+    //will get the index number by counting the dashes
     let occurrences = word.split("-").length - 1;
+    //will get the array based on the index
     let numToIndex = wordList[occurrences];
+    //will remove the dashes
     let newWord = word.replace(/-/gi, "");
 //if the word has more than 7 syllables display an error message
     if (occurrences === 0) {
@@ -44,54 +49,100 @@ function addWord(e) {
     } else if (occurrences === 6) {
         numToIndex.push(newWord);
     } else {
-        $("div#message").text("Sorry! That word has too many syllables for a haiku :(")
+        $("div#message").text("Sorry! That word has too many syllables for a haiku :(");
     }
-    console.log(wordList)
-//hint:use index array
+    console.log(wordList);
 }
 
-//pickRandomWord(syllable)
-//will locate the array with words that have a given number of syllables
-function generateRandomWord(syllable) {
+//generateRandomWord
+function generateRandomWord() {
+    //will create a random number for index 0-6
     let ranOuter = Math.floor(Math.random() * 7);
-    syllableArray.push(ranOuter);
-    let ranIndex = wordList[ranOuter];
-    let innerLength = ranIndex.length;
-    let ranInner = Math.floor(Math.random() * innerLength);
-    row += ranIndex[ranInner].toString();
-    console.log(ranOuter, ranInner);
-    console.log(row);
-
+    //will generate a random number for the array picked
+    let ranInner = Math.floor(Math.random() * wordList[ranOuter].length);
+    //random number pushed to syllable Array, keeps count of syllables
+    syllableArray.push(ranOuter+1);
+    //returns random word
+    let randomWord = wordList[ranOuter][ranInner];
+    row.push(randomWord);
+    //console.log(row);
+    //console.log(syllableArray);
 }
+
+//creates sum of syllableArray
+function addSyllables(){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return syllableArray.reduce(reducer);
+}
+
+//creates rows
+function rowSyllableFive(){
+    generateRandomWord();
+    let sum = addSyllables();
+    do{
+        generateRandomWord();
+        sum = addSyllables();
+        //console.log(addSyllables() + " sum for 5");
+    } while (sum<5);
+}
+function rowSyllableSeven(){
+    generateRandomWord();
+    let sum = addSyllables();
+    do{
+        generateRandomWord();
+        sum = addSyllables();
+        //console.log(addSyllables() + " sum for 7");
+    }while (sum<7);
+}
+
+//clear syllableArray & row
+function clearArray(){
+    row = [];
+    syllableArray = [];
+}
+
+//display haiku
 function generateHaiku(e) {
     e.preventDefault();
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    //I want you to  give me a list of of five random words.
-    for (let i = 0; i < 5; i++) {
-        row += " " , generateRandomWord() + " ";
+    //displays first row
+    rowSyllableFive();
+    let sum = addSyllables();
+    do{
+        clearArray();
+        rowSyllableFive();
+        sum = addSyllables();
+    }while(sum>5);
+    if(sum===5){
+        $("p#row-one").text(row.join(" "));
+        console.log(addSyllables() + " for first row");
+        //console.log(row);
+        clearArray();
     }
-    syllableArray.reduce(reducer);
-    let totalSyllable = syllableArray.reduce(reducer, syllableArray.length);
-
-    console.log(totalSyllable);
-    console.log(syllableArray);
-    console.log(row);
-
-    // If the sum of the words is greater than five I want you to keep keep giving me words util syllables is equal to five
-    if (totalSyllable > 5) {
-       row = "";
-       console.log(row);
-        for (let i = 0; i < 5; i++) {
-            row += " " , generateRandomWord() +" ";
-        }
-        console.log(row);
-    }else if (totalSyllable===5){
-        let rowOne = $("#row-one").text(`${row}`);
-        console.log(rowOne);
-        syllableArray = [];
+    //displays second row
+    rowSyllableSeven();
+    do{
+        clearArray();
+        rowSyllableSeven();
+        sum = addSyllables();
+    }while(sum>7);
+    if(sum===7){
+        $("p#row-two").text(row.join(" "));
+        console.log(addSyllables() + " for second row");
+        //console.log(row);
+        clearArray();
     }
-//I got lost after this and it doesn't even work, how do you build a row >_<
-//display haiku
-
+    //displays third row
+    rowSyllableFive();
+    do{
+        clearArray();
+        rowSyllableFive();
+        sum = addSyllables();
+    }while(sum>5)
+    if(sum===5){
+        $("p#row-three").text(row.join(" "));
+        console.log(addSyllables() + " for third row");
+        //console.log(row);
+        clearArray();
+    }
 }
 
